e0de173a22c7ed3d397c7f7142619ccb5d054ce6
maheshwaran.j <maheshwaran.j@innoppl.com>
2018-02-20 19:34:38 +0530

    social icon

--- modules/floating_social_icons/src/Plugin/Block/FloatingSocialIconsBlock.php
+++ modules/floating_social_icons/src/Plugin/Block/FloatingSocialIconsBlock.php
@@ -27,11 +27,79 @@ class FloatingSocialIconsBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function blockForm($form, FormStateInterface $form_state) {
-    // $form = parent::blockForm($form, $form_state);
+    $form = parent::blockForm($form, $form_state);
 
     $config = $this->getConfiguration();
 
-    $form['simple_social_icons_twitter'] = array(
+    // Facebook details
+    $form['floating_facebook'] = array(
+      '#type' => 'details',
+      '#title' => t('Facebook settings'),
+      '#collapsible' => TRUE,
+      '#collapsed' => TRUE,
+      '#description' => '',
+    );
+    
+
+    $form ['floating_facebook']['link'] = array(
+      '#type' => 'url',
+      '#title' => $this->t('Facebook Link'),
+      '#size' => 40,
+    );
+    
+    $form['floating_facebook']['enable'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Enable facebook icon.'),
+      '#description' => $this->t(''),
+    );
+   
+    // Google Plus details
+    $form['floating_google_plus'] = array(
+      '#type' => 'details',
+      '#title' => t('Google Plus settings'),
+      '#collapsible' => TRUE,
+      '#collapsed' => TRUE,
+      '#description' => '',
+    );
+    
+
+    $form ['floating_google_plus']['link'] = array(
+      '#type' => 'url',
+      '#title' => $this->t('Google Link'),
+      '#size' => 40,
+    );
+    
+    $form['floating_google_plus']['enable'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Enable Google Plus icon.'),
+      '#description' => $this->t(''),
+    );
+
+    // LinkedIn details
+    $form['floating_linkedIn'] = array(
+      '#type' => 'details',
+      '#title' => t('LinkdIn settings'),
+      '#collapsible' => TRUE,
+      '#collapsed' => TRUE,
+      '#description' => '',
+    );
+    
+
+    $form ['floating_linkedIn']['link'] = array(
+      '#type' => 'url',
+      '#title' => $this->t('LinkedIn Link'),
+      '#size' => 40,
+    );
+    
+    $form['floating_linkedIn']['enable'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Enable LinkedIn icon.'),
+      '#description' => $this->t(''),
+    );
+    
+
+    // Twitter details
+    $form['floating_twitter'] = array(
       '#type' => 'details',
       '#title' => t('Twitter settings'),
       '#collapsible' => TRUE,
@@ -39,19 +107,17 @@ class FloatingSocialIconsBlock extends BlockBase {
       '#description' => '',
     );
     
-    $form['simple_social_icons_twitter']['twitter_via'] = array(
-      '#type' => 'textfield',
-      '#title' => t('Via'),
-      '#size' => 60,
-      '#maxlength' => 128,
-      '#weight' => '0',
+
+    $form ['floating_twitter']['link'] = array(
+      '#type' => 'url',
+      '#title' => $this->t('Twitter Link'),
+      '#size' => 40,
     );
     
-    $form['simple_social_icons_twitter']['twitter_enable'] = array(
+    $form['floating_twitter']['enable'] = array(
       '#type' => 'checkbox',
-      '#title' => $this->t('Disable twitter icon.'),
+      '#title' => $this->t('Enable twitter icon.'),
       '#description' => $this->t(''),
-      '#weight' => '1',
     );
 
     return $form;
