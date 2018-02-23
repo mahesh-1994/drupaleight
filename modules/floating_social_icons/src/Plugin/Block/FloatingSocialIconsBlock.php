95b3ac515e83d48395b7bdc6939e011ce8883955
maheshwaran.j <maheshwaran.j@innoppl.com>
2018-02-23 20:38:22 +0530

    array changes

--- modules/floating_social_icons/src/Plugin/Block/FloatingSocialIconsBlock.php
+++ modules/floating_social_icons/src/Plugin/Block/FloatingSocialIconsBlock.php
@@ -29,12 +29,33 @@ class FloatingSocialIconsBlock extends BlockBase {
     $instagram = isset($config['instagram']) ? $config['instagram'] : '';
     $mail = isset($config['mail']) ? $config['mail'] : '';
     $icons = isset($config['icons']) ? $config['icons'] : '';
-    
-    $build = [];
-    $build['#cache']['max-age'] = 0; // No cache
-    $build['#theme'] = 'floating_social_icons_block';
 
-    return $build;
+
+    $social_values = [
+      'facebook' => $facebook,
+      'google_plus' => $google_plus,
+      'linkedIn' => $linkedIn,
+      'twitter' => $twitter,
+      'pinterest' => $pinterest,
+      'instagram' => $instagram,
+      'mail' => $mail,
+      'icons' => $icons,
+    ];
+    
+    // $build = [];
+    // $build['#cache']['max-age'] = 0; // No cache
+    // $build['#theme'] = 'floating_social_icons_block';
+
+    // return $build;
+    
+    // return [
+    //   '#markup' => $this->t('Hello, World!'),
+    // ];
+
+    return [
+      '#theme' => 'floating_social_icons_display',
+      '#social_values' => $social_values,
+    ];
   }
 
   /**
@@ -45,209 +66,173 @@ class FloatingSocialIconsBlock extends BlockBase {
     
     $config = $this->getConfiguration();
 
-    // kint($this->configuration['facebook']);
-
     // Facebook details
-    $form['floating_facebook'] = array(
+    $form['floating_facebook'] = [
       '#type' => 'details',
       '#title' => t('Facebook settings'),
       '#collapsible' => TRUE,
       '#open' => TRUE,
       '#description' => '',
-    );
+    ];
     
 
-    $form ['floating_facebook']['link'] = array(
+    $form ['floating_facebook']['link'] = [
       '#type' => 'url',
       '#title' => $this->t('Facebook Link'),
       '#size' => 60,
       '#default_value' => isset($this->configuration['facebook']) ? $this->configuration['facebook'] : '',
-    );
+    ];
     
-    // $form['floating_facebook']['enable'] = array(
+    // $form['floating_facebook']['enable'] = [
     //   '#type' => 'checkbox',
     //   '#title' => $this->t('Enable facebook icon.'),
     //   '#description' => $this->t(''),
-    // );
+    // ];
 
         // Twitter details
-    $form['floating_twitter'] = array(
+    $form['floating_twitter'] = [
       '#type' => 'details',
       '#title' => t('Twitter settings'),
       '#collapsible' => TRUE,
       '#collapsed' => TRUE,
       '#description' => '',
-    );
+    ];
     
 
-    $form ['floating_twitter']['link'] = array(
+    $form ['floating_twitter']['link'] = [
       '#type' => 'url',
       '#title' => $this->t('Twitter Link'),
       '#size' => 60,
       '#default_value' => isset($this->configuration['twitter']) ? $this->configuration['twitter'] : '',
-    );
+    ];
     
-    // $form['floating_twitter']['enable'] = array(
-    //   '#type' => 'checkbox',
-    //   '#title' => $this->t('Enable twitter icon.'),
-    //   '#description' => $this->t(''),
-    // );
-   
     // Google Plus details
-    $form['floating_google_plus'] = array(
+    $form['floating_google_plus'] = [
       '#type' => 'details',
       '#title' => t('Google Plus settings'),
       '#collapsible' => TRUE,
       '#collapsed' => FALSE,
       '#description' => '',
-    );
+    ];
     
 
-    $form ['floating_google_plus']['link'] = array(
+    $form ['floating_google_plus']['link'] = [
       '#type' => 'url',
       '#title' => $this->t('Google Link'),
       '#size' => 60,
       '#default_value' => isset($this->configuration['google_plus']) ? $this->configuration['google_plus'] : '',
-    );
+    ];
     
-    // $form['floating_google_plus']['enable'] = array(
-    //   '#type' => 'checkbox',
-    //   '#title' => $this->t('Enable Google Plus icon.'),
-    //   '#description' => $this->t(''),
-    // );
-
     // LinkedIn details
-    $form['floating_linkedIn'] = array(
+    $form['floating_linkedIn'] = [
       '#type' => 'details',
       '#title' => t('LinkedIn settings'),
       '#collapsible' => TRUE,
       '#collapsed' => FALSE,
       '#description' => '',
-    );
+    ];
     
 
-    $form ['floating_linkedIn']['link'] = array(
+    $form ['floating_linkedIn']['link'] = [
       '#type' => 'url',
       '#title' => $this->t('LinkedIn Link'),
       '#size' => 60,
       '#default_value' => isset($this->configuration['linkedIn']) ? $this->configuration['linkedIn'] : '',
-    );
+    ];
     
-    // $form['floating_linkedIn']['enable'] = array(
-    //   '#type' => 'checkbox',
-    //   '#title' => $this->t('Enable LinkedIn icon.'),
-    //   '#description' => $this->t(''),
-    // );  
 
     // Pinterest details
-    $form['floating_pinterest'] = array(
+    $form['floating_pinterest'] = [
       '#type' => 'details',
       '#title' => t('Pinterest settings'),
       '#collapsible' => TRUE,
       '#collapsed' => TRUE,
       '#description' => '',
-    );
+    ];
     
 
-    $form ['floating_pinterest']['link'] = array(
+    $form ['floating_pinterest']['link'] = [
       '#type' => 'url',
       '#title' => $this->t('Pinterest Link'),
       '#size' => 60,
       '#default_value' => isset($this->configuration['pinterest']) ? $this->configuration['pinterest'] : '',
-    );
-
-    // $form['floating_pinterest']['enable'] = array(
-    //   '#type' => 'checkbox',
-    //   '#title' => $this->t('Enable Pinterest icon.'),
-    //   '#description' => $this->t(''),
-    // );
+    ];
 
     // Instagram details
-    $form['floating_instagram'] = array(
+    $form['floating_instagram'] = [
       '#type' => 'details',
       '#title' => t('Instagram settings'),
       '#collapsible' => TRUE,
       '#collapsed' => TRUE,
       '#description' => '',
-    );
+    ];
     
 
-    $form ['floating_instagram']['link'] = array(
+    $form ['floating_instagram']['link'] = [
       '#type' => 'url',
       '#title' => $this->t('Instagram Link'),
       '#size' => 60,
       '#default_value' => isset($this->configuration['instagram']) ? $this->configuration['instagram'] : '',
-    );
-
-    // $form['floating_instagram']['enable'] = array(
-    //   '#type' => 'checkbox',
-    //   '#title' => $this->t('Enable Instagram icon.'),
-    //   '#description' => $this->t(''),
-    // );
+    ];
 
     // Mail details
-    $form['floating_mail'] = array(
+    $form['floating_mail'] = [
       '#type' => 'details',
       '#title' => t('Mail settings'),
       '#collapsible' => TRUE,
       '#collapsed' => TRUE,
       '#description' => '',
-    );
+    ];
     
 
-    $form ['floating_mail']['link'] = array(
+    $form ['floating_mail']['link'] = [
       '#type' => 'url',
       '#title' => $this->t('Mail Link'),
       '#size' => 60,
       '#default_value' => isset($this->configuration['mail']) ? $this->configuration['mail'] : '',
-    );
-
-    // $form['floating_mail']['enable'] = array(
-    //   '#type' => 'checkbox',
-    //   '#title' => $this->t('Enable Mail icon.'),
-    //   '#description' => $this->t(''),
-    // );
+    ];
 
     // Block details
-    $form['floating_icons'] = array(
+    $form['floating_icons'] = [
       '#type' => 'details',
       '#title' => t('Display Icons'),
       '#collapsible' => TRUE,
       '#collapsed' => TRUE,
       '#description' => $this->t(''),
-    );
+    ];
     
-    $form['floating_icons']['place'] = array(
+    $form['floating_icons']['place'] = [
       '#type' => 'radios',
       '#title' => $this->t('Where do you want to display the icons'),
-      '#default_value' => isset($this->configuration['icons']) ? $this->configuration['icons'] : 0,
-      '#options' => array(
+      '#default_value' => isset($this->configuration['place']) ? $this->configuration['place'] : 3,
+      '#options' => [
           0 => $this->t('Top'),
           1 => $this->t('Bottom'),
           2 => $this->t('Right'),
           3 => $this->t('Left'),
-      ),
-    );
+      ],
+    ];
 
     return $form;
+  }
+
+  public function blockValidate($form, FormStateInterface $form_state) {
+
   }
 
   public function blockSubmit($form, FormStateInterface $form_state) {
     
     parent::blockSubmit($form, $form_state);
-    
+
     $values = $form_state->getValues();
 
-    $this->configuration['facebook'] = $values['floating_facebook'];
-    $this->configuration['google_plus'] = $values['floating_google_plus'];
-    $this->configuration['linkedIn'] = $values['floating_linkedIn'];
-    $this->configuration['twitter'] = $values['floating_twitter'];
-    $this->configuration['pinterest'] = $values['floating_pinterest'];
-    $this->configuration['instagram'] = $values['floating_instagram'];
-    $this->configuration['mail'] = $values['floating_mail'];
-    $this->configuration['icons'] = $values['floating_icons'];
-
-
-    
+    $this->configuration['facebook'] = $values['floating_facebook']['link'];
+    $this->configuration['google_plus'] = $values['floating_google_plus']['link'];
+    $this->configuration['linkedIn'] = $values['floating_linkedIn']['link'];
+    $this->configuration['twitter'] = $values['floating_twitter']['link'];
+    $this->configuration['pinterest'] = $values['floating_pinterest']['link'];
+    $this->configuration['instagram'] = $values['floating_instagram']['link'];
+    $this->configuration['mail'] = $values['floating_mail']['link'];
+    $this->configuration['place'] = $values['floating_icons']['place'];    
   }
 }
