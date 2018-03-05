ffbeab6e05fa5dae091b5ee5d80f72c7985ce199
maheshwaran.j <maheshwaran.j@innoppl.com>
2018-03-05 20:43:43 +0530

    floating social icons

--- modules/floating_social_icons/src/Plugin/Block/FloatingSocialIconsBlock.php
+++ modules/floating_social_icons/src/Plugin/Block/FloatingSocialIconsBlock.php
@@ -16,20 +16,20 @@ use Drupal\Core\Form\FormStateInterface;
  * )
  */
 class FloatingSocialIconsBlock extends BlockBase {
-// https://spinningcode.org/2017/01/a-pattern-for-drupal-8-blocks/
-  public function build() {
+
+  public function build() {  
 
     // Load the configuration from the form
     $config = $this->getConfiguration();
+
     $facebook = isset($config['facebook']) ? $config['facebook'] : '';
     $google_plus = isset($config['google_plus']) ? $config['google_plus'] : '';
     $linkedIn = isset($config['linkedIn']) ? $config['linkedIn'] : '';
     $twitter = isset($config['twitter']) ? $config['twitter'] : '';
     $pinterest = isset($config['pinterest']) ? $config['pinterest'] : '';
     $instagram = isset($config['instagram']) ? $config['instagram'] : '';
-    $mail = isset($config['mail']) ? $config['mail'] : '';
-    $icons = isset($config['icons']) ? $config['icons'] : '';
-
+    $icons = isset($config['place']) ? $config['place'] : '';
+    $count = isset($config['count']) ? $config['count'] : '';
 
     $social_values = [
       'facebook' => $facebook,
@@ -40,17 +40,15 @@ class FloatingSocialIconsBlock extends BlockBase {
       'instagram' => $instagram,
       'mail' => $mail,
       'icons' => $icons,
+      'count' => $count,
     ];
-    
-    // $build = [];
-    // $build['#cache']['max-age'] = 0; // No cache
-    // $build['#theme'] = 'floating_social_icons_block';
 
-    // return $build;
-    
     return [
       '#theme' => 'floating_social_icons_display',
       '#social_values' => $social_values,
+      '#attached' => [
+      'library' => ['floating_social_icons/floating_social_icons'],
+      ],
     ];
   }
 
@@ -72,7 +70,7 @@ class FloatingSocialIconsBlock extends BlockBase {
     ];
     
 
-    $form ['floating_facebook']['link'] = [
+    $form ['floating_facebook']['facebook_link'] = [
       '#type' => 'url',
       '#title' => $this->t('Facebook Link'),
       '#size' => 60,
@@ -95,7 +93,7 @@ class FloatingSocialIconsBlock extends BlockBase {
     ];
     
 
-    $form ['floating_twitter']['link'] = [
+    $form ['floating_twitter']['twitter_link'] = [
       '#type' => 'url',
       '#title' => $this->t('Twitter Link'),
       '#size' => 60,
@@ -112,7 +110,7 @@ class FloatingSocialIconsBlock extends BlockBase {
     ];
     
 
-    $form ['floating_google_plus']['link'] = [
+    $form ['floating_google_plus']['google_plus_link'] = [
       '#type' => 'url',
       '#title' => $this->t('Google Link'),
       '#size' => 60,
@@ -129,7 +127,7 @@ class FloatingSocialIconsBlock extends BlockBase {
     ];
     
 
-    $form ['floating_linkedIn']['link'] = [
+    $form ['floating_linkedIn']['linkedIn_link'] = [
       '#type' => 'url',
       '#title' => $this->t('LinkedIn Link'),
       '#size' => 60,
@@ -147,7 +145,7 @@ class FloatingSocialIconsBlock extends BlockBase {
     ];
     
 
-    $form ['floating_pinterest']['link'] = [
+    $form ['floating_pinterest']['pinterest_link'] = [
       '#type' => 'url',
       '#title' => $this->t('Pinterest Link'),
       '#size' => 60,
@@ -164,28 +162,11 @@ class FloatingSocialIconsBlock extends BlockBase {
     ];
     
 
-    $form ['floating_instagram']['link'] = [
+    $form ['floating_instagram']['instagram_link'] = [
       '#type' => 'url',
       '#title' => $this->t('Instagram Link'),
       '#size' => 60,
       '#default_value' => isset($this->configuration['instagram']) ? $this->configuration['instagram'] : '',
-    ];
-
-    // Mail details
-    $form['floating_mail'] = [
-      '#type' => 'details',
-      '#title' => t('Mail settings'),
-      '#collapsible' => TRUE,
-      '#collapsed' => TRUE,
-      '#description' => '',
-    ];
-    
-
-    $form ['floating_mail']['link'] = [
-      '#type' => 'url',
-      '#title' => $this->t('Mail Link'),
-      '#size' => 60,
-      '#default_value' => isset($this->configuration['mail']) ? $this->configuration['mail'] : '',
     ];
 
     // Block details
@@ -200,13 +181,20 @@ class FloatingSocialIconsBlock extends BlockBase {
     $form['floating_icons']['place'] = [
       '#type' => 'radios',
       '#title' => $this->t('Where do you want to display the icons'),
-      '#default_value' => isset($this->configuration['place']) ? $this->configuration['place'] : 3,
+      '#required' => TRUE,
+      '#default_value' => isset($this->configuration['place']) ? $this->configuration['place'] : 4,
       '#options' => [
-          0 => $this->t('Top'),
-          1 => $this->t('Bottom'),
+          1 => $this->t('Top'),
           2 => $this->t('Right'),
-          3 => $this->t('Left'),
+          3 => $this->t('Bottom'),
+          4 => $this->t('Left'),
       ],
+    ];
+
+    $form['floating_icons']['count'] = [
+      '#title' =>  t('count'),
+      '#value' => isset($this->configuration['count']) ? $this->configuration['count'] :'',
+      '#type' => 'hidden'
     ];
 
     return $form;
@@ -214,21 +202,44 @@ class FloatingSocialIconsBlock extends BlockBase {
 
   public function blockValidate($form, FormStateInterface $form_state) {
 
+    $values = $form_state->getValues();
+
+    $links = [];
+    $links[] = $values['floating_facebook']['facebook_link'];
+    $links[] = $values['floating_twitter']['twitter_link'];
+    $links[] = $values['floating_google_plus']['google_plus_link'];
+    $links[] = $values['floating_linkedIn']['linkedIn_link'];
+    $links[] = $values['floating_pinterest']['pinterest_link'];
+    $links[] = $values['floating_instagram']['instagram_link'];
+
+    $count = 0;
+    if($links) {
+      foreach ($links as $key => $link) {
+        if(!empty($link)){
+            $count = $count + 1;
+        }      
+      } 
+    }
+    if ($count < 2) {
+      $form_state->setErrorByName('floatingsocialblock',$this->t('Atleast two fields should be filled.'));
+    }
+    // Setting count value
+    $this->configuration['count'] = $count;
   }
+  
 
   public function blockSubmit($form, FormStateInterface $form_state) {
     
     parent::blockSubmit($form, $form_state);
 
     $values = $form_state->getValues();
-
-    $this->configuration['facebook'] = $values['floating_facebook']['link'];
-    $this->configuration['google_plus'] = $values['floating_google_plus']['link'];
-    $this->configuration['linkedIn'] = $values['floating_linkedIn']['link'];
-    $this->configuration['twitter'] = $values['floating_twitter']['link'];
-    $this->configuration['pinterest'] = $values['floating_pinterest']['link'];
-    $this->configuration['instagram'] = $values['floating_instagram']['link'];
-    $this->configuration['mail'] = $values['floating_mail']['link'];
+  
+    $this->configuration['facebook'] = $values['floating_facebook']['facebook_link'];
+    $this->configuration['google_plus'] = $values['floating_google_plus']['google_plus_link'];
+    $this->configuration['linkedIn'] = $values['floating_linkedIn']['linkedIn_link'];
+    $this->configuration['twitter'] = $values['floating_twitter']['twitter_link'];
+    $this->configuration['pinterest'] = $values['floating_pinterest']['pinterest_link'];
+    $this->configuration['instagram'] = $values['floating_instagram']['instagram_link'];
     $this->configuration['place'] = $values['floating_icons']['place'];    
   }
 }
